432b0dacc1f8f07d304a4b3a3917b8e2dc92319a
fil <s267554@studenti.polito.it>
2019-12-07 01:41:22 +0100

    First

---
+++ fortuneserver.cpp
@@ -0,0 +1,589 @@
+/****************************************************************************
+**
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the examples of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** BSD License Usage
+** Alternatively, you may use this file under the terms of the BSD license
+** as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of The Qt Company Ltd nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "fortuneserver.h"
+#include <stdlib.h>
+
+#include <QtNetwork>
+#include <QRandomGenerator>
+#include <QTimer>
+
+
+QDataStream &operator<<(QDataStream& out, const Symbol& sen){
+    QVector<int> qvect;
+    return out << sen.c << sen.count << sen.format << sen.siteid << qvect.fromStdVector(sen.fract);
+}
+QDataStream &operator>>(QDataStream& in, Symbol& rec){
+    QVector<int> qvect;
+    in >> rec.c >> rec.count >> rec.format >> rec.siteid >> qvect;
+    rec.fract = qvect.toStdVector();
+    return in;
+}
+
+QDataStream &operator<<(QDataStream& out, const Message& sen){
+    return out << sen.totAdd << sen.totRem << sen.genFrom << sen.symToAdd << sen.symToRem;
+}
+QDataStream &operator>>(QDataStream& in, Message& rec){
+    return in >> rec.totAdd >> rec.totRem >> rec.genFrom >> rec.symToAdd >> rec.symToRem;
+}
+
+
+QDataStream &operator<<(QDataStream& out, const NotifyCursor& sen){
+    return out << sen.uid << sen.cursPos;
+}
+QDataStream &operator>>(QDataStream& in, NotifyCursor& rec){
+    return in >> rec.uid >> rec.cursPos;
+}
+
+QDataStream &operator<<(QDataStream& out, const User& sen){
+    return out << sen.uid << sen.icon << sen.nick << sen.color << sen.startCursor;
+}
+QDataStream &operator>>(QDataStream& in, User& rec){
+    return in >> rec.uid >> rec.icon >> rec.nick >> rec.color >> rec.startCursor;
+}
+
+QDataStream &operator<<(QDataStream& out, const Account& sen){
+    return out << sen.password << sen.uinqueId << sen.username;
+}
+QDataStream &operator>>(QDataStream& in, Account& rec){
+    return in >> rec.password >> rec.uinqueId >> rec.username;
+}
+
+template<class T>
+QDataStream &operator<<(QDataStream& stream, const std::vector<T>& val){
+    stream << static_cast<quint32>(val.size());
+    for(auto& singleVal : val)
+        stream << singleVal;
+    return stream;
+}
+
+template<class T>
+QDataStream &operator>>(QDataStream& stream, std::vector<T>& val){
+    quint32 vecSize;
+    val.clear();
+    stream >> vecSize;
+    val.reserve(vecSize);
+    T tempVal;
+    while(vecSize--){
+        stream >> tempVal;
+        val.push_back(tempVal);
+    }
+    return stream;
+}
+
+
+FortuneServer::FortuneServer(QObject *parent)
+    : QTcpServer(parent)
+{
+    QFile accounts("accounts");
+    if(accounts.open(QIODevice::ReadOnly)){
+        QDataStream in(&accounts);
+        in.setVersion(QDataStream::Qt_4_0);
+        in >> _accounts;
+    }
+    accounts.close();
+
+    QFile profiles("profiles");
+    if(profiles.open(QIODevice::ReadOnly)){
+        QDataStream in(&profiles);
+        in.setVersion(QDataStream::Qt_4_0);
+        in >> _profiles;
+    }
+    profiles.close();
+
+    _filenames = retrieveFiles();
+}
+
+void FortuneServer::incomingConnection(qintptr socketDescriptor)
+{
+    ClientConn * client = new ClientConn(socketDescriptor, this);
+    // next pending connection LOOK QT DOCS
+}
+
+quint32 FortuneServer::checkCredentials(QString username, QString password)
+{
+    for (auto acc : _accounts) {
+        if (acc.username == username && acc.password == password) {        
+            return acc.uinqueId;
+        }
+    }
+
+    return 0;
+}
+
+quint32 FortuneServer::registerUser(QString username, QString password)
+{
+    quint32 uid = QRandomGenerator::global()->generate();
+
+    for (auto acc : _accounts) {
+        if (acc.username == username) {
+            return 0;
+        }
+        while (acc.uinqueId==uid && uid!=0) {
+            uid = QRandomGenerator::global()->generate();
+        }
+    }
+
+    Account a;
+
+    a.password = password;                      // create decent constructor here
+    a.username = username;
+    a.uinqueId = uid;
+
+    _accounts.append(a);
+
+    QFile accounts("accounts");
+    accounts.open(QIODevice::WriteOnly);
+    QDataStream out1(&accounts);
+    out1.setVersion(QDataStream::Qt_4_0);
+
+    out1 << _accounts;
+    accounts.close();
+
+    // generate random profile
+
+    User randU(uid,
+               QString(QRandomGenerator::global()->generate()),
+               QColor(QRandomGenerator::global()->generate() % 128 +128,QRandomGenerator::global()->generate() % 128 +128,QRandomGenerator::global()->generate() % 128 +128),
+               0);
+
+    _profiles.insert(uid,randU);
+
+    QFile profiles("profiles");
+    profiles.open(QIODevice::WriteOnly);
+    QDataStream out2(&profiles);
+    out2.setVersion(QDataStream::Qt_4_0);
+
+    out2 << _profiles;
+    profiles.close();
+
+    return uid;
+}
+
+QStringList FortuneServer::retrieveFiles()
+{
+    QDir docs("documents");
+    return docs.entryList();
+}
+
+Document * FortuneServer::openFile(QString fname, ClientConn* client)
+{
+    if(_opendocs.contains(fname)){
+        _opendocs.find(fname).value()->newSub(client);
+        return _opendocs.find(fname).value();
+    }
+    else {
+        Document * doc = new Document(fname, client);
+        _opendocs.insert(fname, doc);
+        return doc;
+    }
+}
+
+Document * FortuneServer::newFile(QString fname, ClientConn* client)
+{
+    Document * doc = new Document(fname, client);
+    _opendocs.insert(fname, doc);
+
+    _filenames.append(fname);
+
+    return doc;
+
+}
+
+void FortuneServer::updateUser(User user, quint32 uid)
+{
+    if(_profiles.contains(uid)){
+        _profiles.insert(uid, user);
+    }
+    else {
+        _profiles.find(uid).value() = user;
+    }
+
+    QFile profiles("profiles");
+    profiles.open(QIODevice::WriteOnly);
+    QDataStream out(&profiles);
+    out.setVersion(QDataStream::Qt_4_0);
+
+    out << _profiles;
+    profiles.close();
+}
+
+User FortuneServer::getProfile(quint32 uid)
+{
+    return _profiles.find(uid).value();
+}
+
+
+ClientConn::ClientConn(int socketDescriptor, FortuneServer * server) : server(server)
+{
+    tcpSock = new QTcpSocket();
+    tcpSock->setSocketDescriptor(socketDescriptor);
+
+    connect(tcpSock, &QTcpSocket::readyRead, this, &ClientConn::readAnyMessage);
+
+}
+
+ClientConn::~ClientConn()
+{
+
+}
+
+void ClientConn::readAnyMessage()
+{
+    QDataStream in(tcpSock);
+    in.setVersion(QDataStream::Qt_4_0);
+
+    int op;
+
+    QString username;
+    QString password;
+
+    QString filename;
+    QString answer;
+
+    User user;
+
+    NotifyCursor nfy;
+    Message msg;
+
+    do {
+        in.startTransaction();
+        in >> op;
+        switch(op) {
+        case 'l':
+            in >> username;
+            in >> password;
+            uniqueId = server->checkCredentials(username, password);
+            if (uniqueId != 0) {
+                isLoggedIn = true;
+                clientPro = server->getProfile(uniqueId);
+
+                QByteArray block;
+                QDataStream out(&block, QIODevice::WriteOnly);
+                out.setVersion(QDataStream::Qt_4_0);
+
+                out << op;
+                out << uniqueId;
+                out << server->retrieveFiles();
+
+                tcpSock->write(block);
+            }
+            else {
+                QByteArray block;
+                QDataStream out(&block, QIODevice::WriteOnly);
+                out.setVersion(QDataStream::Qt_4_0);
+                out << op;
+
+                out << uniqueId;
+                tcpSock->write(block);
+            }
+            break;
+        case 's':
+            in >> username;
+            in >> password;
+            uniqueId = server->registerUser(username, password);
+            if (uniqueId != 0) {
+                isLoggedIn = true;
+                clientPro = server->getProfile(uniqueId);
+
+                QByteArray block;
+                QDataStream out(&block, QIODevice::WriteOnly);
+                out.setVersion(QDataStream::Qt_4_0);
+
+                out << op;
+                out << uniqueId;
+                out << server->retrieveFiles();
+
+                tcpSock->write(block);
+            }
+            else {
+                QByteArray block;
+                QDataStream out(&block, QIODevice::WriteOnly);
+                out.setVersion(QDataStream::Qt_4_0);
+
+                out << op;
+                out << uniqueId;
+                tcpSock->write(block);
+            }
+            break;
+        case 'o':
+            in >> filename;
+            workingOn = server->openFile(filename, this);
+            break;
+        case 'n':
+            in >> filename;
+            workingOn = server->newFile(filename, this);
+            break;
+        case 'u':
+            in >> user;
+            server->updateUser(user, uniqueId);
+            break;
+        case 'm':
+            in >> msg;
+            workingOn->process(msg);
+            break;
+        case 'c':
+            in >> nfy;
+            workingOn->relay(nfy);
+            break;
+        }
+    } while(in.commitTransaction());
+
+}
+
+
+Document::Document(QString fname, ClientConn* client)
+{
+    file.setFileName(QString("documents/").append(fname));
+    file.open(QFile::ReadWrite);
+
+    QDataStream in(&file);
+    in.setVersion(QDataStream::Qt_4_0);
+    in >> _symbols;
+    newSub(client);
+    file.close();
+
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &Document::autoSave);
+    timer->start(1000);
+}
+
+void Document::newSub(ClientConn *sub)
+{
+    quint32 uid = sub->uniqueId;
+
+    _subs.insert(uid, sub);
+
+    connect(sub->tcpSock, &QTcpSocket::disconnected, [this, uid]()
+    {
+        byeUser(uid);
+    });
+
+    // send content to new sub
+    QByteArray block;
+    QDataStream out(&block, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_4_0);
+
+    out << 't';
+    out << _symbols;
+    sub->tcpSock->write(block);
+
+    // send new sub's User to every sub
+    QByteArray blockU;
+    QDataStream outU(&blockU, QIODevice::WriteOnly);
+    outU.setVersion(QDataStream::Qt_4_0);
+    outU << 'u';
+    outU << sub->clientPro;
+
+    for(auto any: _subs){
+        any->tcpSock->write(blockU);
+    }
+
+    // send any sub's User to new sub
+
+    for(auto any: _subs){
+        if(any->uniqueId!=uid){
+            QByteArray blockN;
+            QDataStream outN(&blockN, QIODevice::WriteOnly);
+            outN.setVersion(QDataStream::Qt_4_0);
+            outN << 'u';
+            outN << any->clientPro;
+            sub->tcpSock->write(blockN);
+        }
+    }
+
+
+}
+
+void Document::autoSave()
+{
+    file.open(QFile::WriteOnly | QFile::Truncate);
+    QDataStream out(&file);
+    out.setVersion(QDataStream::Qt_4_0);
+
+    out << _symbols;
+
+    file.close();
+}
+
+void Document::byeUser(quint32 uid)
+{
+    for(auto client : _subs){
+        if(client->uniqueId != uid){
+            QByteArray block;
+            QDataStream out(&block, QIODevice::WriteOnly);
+            out.setVersion(QDataStream::Qt_4_0);
+            out << 'd';
+            out << uid;
+            client->tcpSock->write(block);
+        }
+    }
+}
+
+void Document::process(const Message& m)
+{
+
+//    QString textA;
+//    for(auto s : m.symToAdd){
+//        textA.append(s.c);
+//    }
+//    qDebug() << "add is: " << textA;
+
+//    QString textR;
+//    for(auto s : m.symToRem){
+//        textR.append(s.c);
+//    }
+//    qDebug() << "rem is: " << textR;
+
+
+    for(auto mi = m.symToRem.begin(); mi != m.symToRem.end(); mi++){
+        int i = 0;
+        for (auto it = _symbols.begin(); it != _symbols.end(); it++) {
+            if (it->siteid == mi->siteid && it->count == mi->count) {
+
+                _symbols.erase(it);
+
+                break;
+            }
+            i++;
+        }
+    }
+
+    for(auto mi = m.symToAdd.begin(); mi != m.symToAdd.end(); mi++){
+        int i;
+        bool found = false;
+
+        for (i = 0; i < _symbols.size() && !found; i++) {
+            bool next = false;
+            auto curr = _symbols.at(i).fract;
+            int digit = 0;
+
+            while (!found && !next &&
+                        curr.size() > digit && mi->fract.size() > digit)
+            {
+                if (curr.at(digit) > mi->fract.at(digit)) {
+                    found = true;
+                    i--;
+                }
+                if(curr.at(digit) < mi->fract.at(digit)){
+                    next = true;
+                }
+                digit++;
+            }
+
+            // until now vectors are equal
+            if(!found && !next) {
+                // maybe one of the two vector continues
+                if(curr.size() > digit) {
+                    if(curr.at(digit) > 0){
+                        found = true;
+                        i--;
+                    }
+                }
+                else if (mi->fract.size() > digit) {
+                    if(mi->fract.at(digit) == 0){
+                        found = true;
+                        i--;
+                    }
+                }
+                else {
+                    // identical fract, then I look at _site Id than at count
+                    if(_symbols.at(i).siteid > mi->siteid){
+                        found = true;
+                        i--;
+                    }
+                    else if(_symbols.at(i).siteid < mi->siteid){
+                        found = true;
+                    }
+                    else {
+                        if(_symbols.at(i).count > mi->count){
+                            found = true;
+                            i--;
+                        }
+                        else if(_symbols.at(i).count < mi->count){
+                            found = true;
+                        }
+                        // I shouldn't be here, it's the same symbol!!!!
+                    }
+                }
+            }
+        }
+
+        _symbols.insert(_symbols.begin() + i, *mi);
+
+    }
+
+    for(auto client : _subs){
+        if(client->uniqueId != m.genFrom){
+            QByteArray block;
+            QDataStream out(&block, QIODevice::WriteOnly);
+            out.setVersion(QDataStream::Qt_4_0);
+            out << 'm';
+
+            out << m;
+            client->tcpSock->write(block);
+        }
+    }
+}
+
+void Document::relay(const NotifyCursor & nfy)
+{
+    for(auto client : _subs){
+        if(client->uniqueId != nfy.uid){
+            QByteArray block;
+            QDataStream out(&block, QIODevice::WriteOnly);
+            out.setVersion(QDataStream::Qt_4_0);
+            out << 'c';
+
+            out << nfy;
+            client->tcpSock->write(block);
+        }
+    }
+}
---
+++ threadedfortuneserver.pro
@@ -0,0 +1,10 @@
+QT += network widgets core
+
+HEADERS       = dialog.h \
+                fortuneserver.h
+SOURCES       = dialog.cpp \
+                fortuneserver.cpp \
+                main.cpp
+
+
+
